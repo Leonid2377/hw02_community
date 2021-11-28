d10916c13cd2f16ce588ac8bde0a853c297b6cae
Leonid Starostin <leonid2377@mail.ru>
2021-11-28 15:38:59 +0300

    исправление

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,11 +5,12 @@ User = get_user_model()
 
 
 class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
     def __str__(self):
         return self.title
-    title = models.CharField(max_length=200)
-    slug = models.SlugField()
-    description = models.TextField()
 
 
 class Post(models.Model):
