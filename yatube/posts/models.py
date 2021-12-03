d24ce6444fd11e877381e18b4091752710cad369
Leonid Starostin <leonid2377@mail.ru>
2021-12-03 06:59:06 +0300

    исправление, на ревью 5

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='group_posts'
+        related_name='posts'
     )
 
     class Meta:
